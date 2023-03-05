2b9e2b21cef72f0b2227acff5ccb20a616aa4638
sitompuledward180801@hotmail.com <sitompuledward180801@hotmail.com>
2023-03-05 20:08:08 +0700

    Add project files.

---
+++ BlogAku/Data/MysqlContext.cs
@@ -0,0 +1,15 @@
+﻿using BlogAku.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogAku.Data
+{
+    public class MysqlContext:DbContext
+    {
+        public MysqlContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
